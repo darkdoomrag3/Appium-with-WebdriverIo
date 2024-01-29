31a9223d416b27386f4cda7c72d14a75dd154471
Emad Deym <47889899+darkdoomrag3@users.noreply.github.com>
2024-01-30 00:30:23 +0330

    Initial commit

---
+++ test/specs/sample.js
@@ -0,0 +1,9 @@
+import { expect } from '@wdio/globals'
+
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await driver.pause(6000)
+    })
+})
+
